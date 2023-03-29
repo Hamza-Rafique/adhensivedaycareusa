d843c996eecae412343bd669bf9ee7b840b9c51c
Hamza Rafique <hamzarafique964@gmail.com>
2023-03-29 11:16:22 +0500

    changes

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Slider from "react-slick";
+import './slider.css'
 
 function ImageSlider() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+    speed: 3000,
+    slidesToShow: 2,
     slidesToScroll: 1,
   };
 
@@ -16,19 +17,19 @@ function ImageSlider() {
       <Slider {...settings}>
         <div>
           <img
-            src="https://source.unsplash.com/WLUHO9A_xik/800x600"
+            src="http://img1.wsimg.com/isteam/stock/74pag1V/:/rs=w:1200,cg:true,m"
             alt="school children"
           />
         </div>
         <div>
           <img
-            src="https://source.unsplash.com/yYwHqXEJqfA/800x600"
+            src="http://img1.wsimg.com/isteam/stock/7734/:/rs=w:1200,cg:true,m"
             alt="school children"
           />
         </div>
         <div>
           <img
-            src="https://source.unsplash.com/7vSjXlcgE7s/800x600"
+            src="http://img1.wsimg.com/isteam/stock/xqgp9aG/:/rs=w:1200,cg:true,m"
             alt="school children"
           />
         </div>
